12d3086c9d43160663c1a6081373a9ac4b510c21
git <borna.gajic2@gmail.com>
2020-11-26 20:21:52 +0100

    FactoryMethod

---
+++ C#/DesignPatterns/FactoryMethod/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace FactoryMethod
+{
+	// Creator
+	public abstract class Logistics
+	{
+		public abstract ITransport CreateTransport ();
+
+		// Other methods that both logistics can use
+	}
+
+	// Concrete creators
+	public class LandLogistics : Logistics
+	{
+		override public ITransport CreateTransport ()
+		{
+			return new Truck();
+		}
+
+		// something specific before creating a new Truck
+	}
+
+	public class SeaLogistics : Logistics
+	{
+		public override ITransport CreateTransport ()
+		{
+			return new Ship();
+		}
+
+		// something specific before creating a new Ship
+	}
+
+
+	// Product
+	public interface ITransport
+	{
+		void Deliver ();	
+	}
+
+	// Concrete products
+	public class Truck : ITransport
+	{
+		public void Deliver()
+		{
+			Console.WriteLine("Delivery by land.");
+		}
+	}
+
+	public class Ship : ITransport
+	{
+		public void Deliver()
+		{
+			Console.WriteLine("Delivery by sea.");
+		}
+	}
+
+	class Program
+	{
+		public static void SomethingAfterDelivery (Logistics logistics)
+		{
+			logistics.CreateTransport().Deliver();
+		}
+
+		static void Main (string[] args)
+		{
+			string line = Console.ReadLine();
+
+			if (line.ToLower() == "land")
+			{
+				SomethingAfterDelivery(new LandLogistics());
+			}
+			else if (line.ToLower() == "sea")
+			{
+				SomethingAfterDelivery(new SeaLogistics());
+			}
+		}
+	}
+}
